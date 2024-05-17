218183c1d46deeb5bfea702378bce10e1e090821
Remi Lavigne <remilavigne.pro@gmail.com>
2024-05-17 08:44:53 +0200

    set-up completed and first marketing page added

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+//cn function will enable us to use tailwind classes in our components
+
